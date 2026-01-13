a38bb2011cdf49e50fd9416b94b45243f930386f
Lakshmanan.A <30818666+Laaxxmm@users.noreply.github.com>
2026-01-13 18:32:31 +0530

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { motion, useScroll, useTransform, useInView, AnimatePresence } from "framer-motion";
 import {
-    Cow,
+    Milk,
     HeartHandshake,
     Recycle,
     Users,
@@ -447,9 +447,9 @@ const SustainabilitySection = () => {
                             <p className="text-Foreground mt-2 text-[clamp(1rem,2vw,1.25rem)]">We use sustainable farming methods and minimize waste.</p>
                         </AnimatedCard>
                         <AnimatedCard>
-                            <Cow className="text-Primary w-8 h-8 mb-2" />
+                            <Milk className="text-Primary w-8 h-8 mb-2" />
                             <h3 className="text-Foreground font-bold text-[clamp(1.5rem,3vw,2rem)]">Ethical Treatment</h3>
-                            <p className="text-Foreground mt-2 text-[clamp(1rem,2vw,1.25rem)]">We ensure the well-being of our cows and promote humane practices.</p>
+                            <p className="text-Foreground mt-2 text-[clamp(1rem,2vw,1.25rem)]">We ensure the well-being of our Milks and promote humane practices.</p>
                         </AnimatedCard>
                     </div>
                 </motion.div>
